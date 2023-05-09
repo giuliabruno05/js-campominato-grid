2e4f71b88300e6e83be8da500af3a930c9b6aa8a
giuliabruno05 <giuliabruno48@gmail.com>
2023-05-09 18:36:41 +0200

    aggiunta il bonus dei tre livelli di difficoltà

--- js/main.js
+++ js/main.js
@@ -1,36 +1,47 @@
+//dichiaro una variabile
+let numArray = 100;
+
+
 // costanti globali
 const grid = document.getElementById("grid")
 const buttonPlay = document.getElementById("button")
-const numArray = 100;
 const array = Array.from({length:numArray},(_,i) => i + 1);
+
 
 
 //L'utente clicca il bottone per generare una griglia
 buttonPlay.addEventListener("click",
     function () {
-        for (let i = 0; i < 100; i++) {
-            const newSquare = createGridSquare("div","square");
-            console.log(newSquare);
-            newSquare.addEventListener("click",
+        
+        // salvo la difficoltà del livello
+
+        const levelPlay = document.getElementById("level").value;
+        if (levelPlay === "easy") {
+
+            nameClass="easy";
+
+            generaGridSquare();
+
+        } else if (levelPlay === "medium") {// SE il livello scelto è medium la griglia diventa da 9
+            numArray = 81;
+
+            nameClass="medium";
+
+            generaGridSquare();
             
-            function () {
-                newSquare.classList.add("squareBlue");
-                console.log(array[i]);
-            }
-            )
+        } else {//SE il livello scelto è hard la griglia diventa da 7
+            numArray = 49;
             
-            grid.append(newSquare);
+            nameClass="hard";
 
-            newSquare.append(array[i]);
-
-
-
+            generaGridSquare();
         }
+        
 }
    
 )
 
-//click per cambiare colore al quadrato
+
 
 
 
@@ -40,4 +51,22 @@ function createGridSquare(tagType,nameClass) {
     const newElement = document.createElement(tagType);
     newElement.classList.add(nameClass);
     return newElement;
+}
+// funzione per generare la grigli per tutti livelli
+function generaGridSquare() {
+    for (let i = 0; i < numArray; i++) {
+        const newSquare = createGridSquare("div",nameClass);
+
+        newSquare.addEventListener("click",
+        
+        function () {
+            newSquare.classList.add("squareBlue");
+            console.log(array[i]);
+        }
+        )
+        
+        grid.append(newSquare);
+
+        newSquare.append(array[i]);
+    }
 }
